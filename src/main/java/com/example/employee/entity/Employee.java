3a439a20fa13bb236f9e50c28ca280c3f6daa8fa
mazerong <chdmazr@126.com>
2019-05-02 13:06:00 +0800

    complete!

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,81 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @NotNull
+    private String name;
+    @NotNull
+    private Integer age;
+    @NotNull
+    private String gender;
+    @NotNull
+    private Integer companyId;
+    @NotNull
+    private Integer salary;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, @NotNull String name, @NotNull Integer age, @NotNull String gender, @NotNull Integer companyId, @NotNull Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
 }
